f77bcb11b2ee15525933268c1c6f5f9144a08542
Peter Lodri <silentpocok@gmail.com>
2016-05-17 15:15:55 +0200

    modified to match PEP-8

--- week-03/day-1/palindrome.py
+++ week-03/day-1/palindrome.py
@@ -1,7 +1,6 @@
 def palindrome(word):
 	print(word + word[::-1])
 
-
 word_in = input("Enter a word: ")
 
 palindrome(word_in)
--- week-03/day-1/palindrome_search.py
+++ week-03/day-1/palindrome_search.py
@@ -1,11 +1,11 @@
-w = input("Enter words separated by space: ")
-
-word_list = w.split()
-
 def search(words):
 	for word in words:
 		if word != word[::-1]:
 			words.remove(word)
 	print(words)
+
+w = input("Enter words separated by space: ")
+
+word_list = w.split()
 
 search(word_list)
