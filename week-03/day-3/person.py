2e71a762c9957ef023305e4e273a6719df78017e
Peter Lodri <silentpocok@gmail.com>
2016-05-19 14:39:19 +0200

    student greet + average

--- week-03/day-3/person.py
+++ week-03/day-3/person.py
@@ -1,6 +1,6 @@
 class Person:
 
-	def __init__(self, first="Tomi", last="Géza"):
+	def __init__(self, first="", last=""):
 		self.first = first
 		self.last = last
 
@@ -8,5 +8,26 @@ class Person:
 		print("{} {}".format(self.first, self.last))
 
 
-p = Person()
-p.greet()
+class Student(Person):
+
+	def __init__(self, grades=[], first="", last=""):
+		super(Student, self).__init__(first="", last="")
+		self.grades = grades
+		self.first = first
+		self.last = last
+
+	def add_grades(self, grade):
+		grade = int(grade)
+		return self.grades.append(grade)
+
+	def salute(self):
+		average = sum(self.grades) / len(self.grades)
+		print("{} {}, avarage: {}".format(self.first, self.last, average))
+
+
+s = Student(first="Peter", last="Lodri")
+s.add_grades(5)
+s.add_grades(1)
+s.add_grades(2)
+s.add_grades(3)
+s.salute()
