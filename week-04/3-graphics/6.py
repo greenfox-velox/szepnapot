2218ba5116105194ebe754a4de1a68c4581e8c90
Peter Lodri <silentpocok@gmail.com>
2016-05-25 10:14:16 +0200

    circle in the middle

---
+++ week-04/3-graphics/6.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# create a 300x300 canvas.
+# draw a green 10x10 square to its center.
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+canvas.create_oval(140, 140, 160, 160, fill="green")
+
+root.mainloop()
