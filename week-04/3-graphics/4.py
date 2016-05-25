8b869e5cbb611e297cdc1c8c75e83bb62b55bc3f
Peter Lodri <silentpocok@gmail.com>
2016-05-25 09:56:51 +0200

    draw_line function

---
+++ week-04/3-graphics/4.py
@@ -0,0 +1,30 @@
+from tkinter import *
+
+# create a 300x300 canvas.
+# create a line drawing function that takes 2 parameters:
+# the x and y coordinates of the line's starting point
+# and draws a line from that point to the center of the canvas.
+# draw 3 lines with that function.
+
+class Example(Canvas):
+
+	def __init__(self):
+		Canvas.__init__(self)
+
+	def draw_line(self, x, y):
+		return canvas.create_line(x, y, 150, 150)
+
+
+if __name__ == '__main__':
+	root = Tk()
+	canvas = Canvas(root, width='300', height='300')
+	E = Example()
+	E.draw_line(100, 50)
+	E.draw_line(20, 75)
+	E.draw_line(230, 100)
+	canvas.pack()
+	root.mainloop()
+
+
+
+
