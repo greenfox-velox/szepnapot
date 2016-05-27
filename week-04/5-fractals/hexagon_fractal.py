9c875a519977f28fa73f926ab957fd3e3e86084f
Peter Lodri <silentpocok@gmail.com>
2016-05-27 13:50:18 +0200

    added random color function

--- week-04/5-fractals/hexagon_fractal.py
+++ week-04/5-fractals/hexagon_fractal.py
@@ -7,32 +7,23 @@ HEIGHT = 800
 
 root = Tk()
 
-COLORS = ['red', 'orange red', 'tomato', 'dark orange', 'orange',
-					'brown', 'firebrick', 'chocolate', 'wheat', 'beige', 'burlywood',
-					'saddle brown', 'indian red', 'rosy brown', 'light goldenrod', 'gold',
-					'yellow', 'yellow green', 'lime green', 'green yellow', 'green',
-					'lawn green', 'spring green', 'sea green', 'aquamarine', 'turquoise',
-					'light blue', 'deep sky blue', 'light slate blue', 'navy', 'deep pink',
-					'orchid', 'purple']
+def gen_hex_colour_code():
+   return '#' + ''.join([random.choice('0123456789ABCDEF') for x in range(6)])
 
-canvas = Canvas(root, width=WIDTH, height=HEIGHT, bg=random.choice(COLORS))
+canvas = Canvas(root, width=WIDTH, height=HEIGHT, bg=gen_hex_colour_code())
 
-def draw_hex():
-	pass
-
-
-def create_hex(topx, topy, size, fill=random.choice(COLORS)):
-	ma = math.sqrt(3) / 2 * size
-	if size < 9:
-		return
-	canvas.create_polygon(topx, topy, topx + size, topy, topx + size + size/2, topy + ma, topx + size, topy + ma*2,
-												topx, topy + ma*2, topx - size/2, topy + ma, topx, topy, fill=fill, outline="black")
-	create_hex(topx, topy, size/3, fill=random.choice(COLORS))
-	create_hex(topx + size/3*2, topy, size / 3, fill=random.choice(COLORS))
-	create_hex(topx + size, 2/3 * ma + topy, size/3, fill=random.choice(COLORS))
-	create_hex(topx + size/3*2, topy + 4/3 * ma, size/3, fill=random.choice(COLORS))
-	create_hex(topx, topy + 4/3 * ma, size/3, fill=random.choice(COLORS))
-	create_hex(topx - size/3, 2/3 * ma + topy, size/3, fill=random.choice(COLORS))
+def create_hex(topx, topy, size, fill=gen_hex_colour_code()):
+  ma = math.sqrt(3) / 2 * size
+  if size < 9:
+    return
+  canvas.create_polygon(topx, topy, topx + size, topy, topx + size + size/2, topy + ma, topx + size, topy + ma*2,
+                        topx, topy + ma*2, topx - size/2, topy + ma, topx, topy, fill=fill, outline=fill)
+  create_hex(topx, topy, size/3, fill=gen_hex_colour_code())
+  create_hex(topx + size/3*2, topy, size / 3, fill=gen_hex_colour_code())
+  create_hex(topx + size, 2/3 * ma + topy, size/3, fill=gen_hex_colour_code())
+  create_hex(topx + size/3*2, topy + 4/3 * ma, size/3, fill=gen_hex_colour_code())
+  create_hex(topx, topy + 4/3 * ma, size/3, fill=gen_hex_colour_code())
+  create_hex(topx - size/3, 2/3 * ma + topy, size/3, fill=gen_hex_colour_code())
 
 
 create_hex(300, 50, 300)
